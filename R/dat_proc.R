d96988c0972c51726d58d7743b5e2c6ea2b48791
fawda123 <mbafs2012@gmail.com>
2021-10-24 13:28:02 -0400

    processed ccha raw data

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -16,5 +16,55 @@ drive_deauth()
 # data here https://drive.google.com/drive/u/0/folders/1ZbpbBfIxb5-BnXhYjymVNMzB88DqZeAO
 id <- '1x_ytLD6ro--QzcCClnDvFC04m7PmbVbtReVQkNktyd4'
 
-yr1 <- read_sheet(id, sheet = 'Yr1-transect')
-yr2 <- read_sheet(id, sheet = 'Yr2-transect')
+yr1 <- read_sheet(id, sheet = 'Yr1-transect') %>% 
+  mutate(sample = 'one')
+yr2 <- read_sheet(id, sheet = 'Yr2-transect') %>% 
+  mutate(sample = 'two') %>% 
+  rename(meter = Meter)
+
+# combine, minor data edits
+cchadat <- bind_rows(yr1, yr2) %>% 
+  rename(
+    species = Vegetation_species, 
+  ) %>% 
+  rename_all(tolower) %>% 
+  mutate(
+    date = as.Date(date), 
+    species = case_when(
+      species == 'Acrostichum danaeifolium' ~ 'Acrostichum danaefolium', 
+      species == 'Baccahris sp.' ~ 'Baccharis sp.', 
+      species == 'Mikania scadens' ~ 'Mikania scandens', 
+      species == 'Monoanthochloe littoralis' ~ 'Monanthochloe littoralis', 
+      species == 'Myrica cerifera' ~ 'Morella cerifera', 
+      species == 'Open water' ~ 'Open Water', 
+      species == 'Salicornia virginicus' ~ 'Salicornia virginica', 
+      species %in% c('Unknown 1', 'Unknown vine') ~ 'Unknown',
+      species == 'Woody debris' ~ 'Woody Debris', 
+      T ~ species
+    ), 
+    zone_name = case_when(
+      zone_name == 'Salt barren' ~ 'Salt Barren',
+      zone_name == 'Schinus terebinthifolia' ~ 'Schinus terebinthifolius',
+      T ~ zone_name
+    ), 
+    pcent_basal_cover = case_when(
+      pcent_basal_cover > 100 ~ NA_real_, 
+      pcent_basal_cover < 0 ~ NA_real, 
+      T ~ pcent_basal_cover
+    )
+  )
+
+save(cchadat, file = here('data/cchadat.RData'))
+
+# cchaloc <- read_sheet(id, sheet = 'ZoneData') %>% 
+#   select(
+#     site = Site, 
+#     lng = longitude, 
+#     lat = latitude
+#     ) %>% 
+#   group_by(site) %>% 
+#   summarise(
+#     lng = mean(lng), 
+#     lat = mean(lat), 
+#     .groups = 'drop'
+#   )
